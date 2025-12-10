9c138c6087585c0014126ae9e11e6f4cbabc5bc7
ViolettHub <violetscriptss2@gmail.com>
2025-12-10 13:10:51 -0300

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,5 +1,5 @@
 return {
-    Background = "https://linkdafoto.com/imagem.png",
+    Background = "https://media.discordapp.net/attachments/1443353629095624825/1448042910355554367/y4rxlrrffxef1.jpeg?ex=693a7b4d&is=693929cd&hm=bdfe750c52fd1cce6b04fcf8dd3e67238b24c0806377e0e20d32a6d4b2f1135d&=&format=webp&width=601&height=930",
     Theme = "Default",
     Title = "Mai Hub",
     Author = "Luna",
