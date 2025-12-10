8696a9474c5811a928ff41dac45d4e13770951bf
ViolettHub <violetscriptss2@gmail.com>
2025-12-10 14:02:06 -0300

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,5 +1,5 @@
 return {
-    Background = "https://media.discordapp.net/attachments/1443353629095624825/1448039212707221615/AAAiew.JPG?ex=693a77db&is=6939265b&hm=98d6379c5233b8523eec07f71e9ed6d8103a8cd4de5503d48c5ec8d1a9dbb544&=&format=webp&width=680&height=513",
+    Background = "https://media.discordapp.net/attachments/1443353629095624825/1448358306849751203/da8050322d4e962ffa68dab9b0bfce50.png?ex=693af849&is=6939a6c9&hm=b9627468618092ec97e0568d7db8a8034d205e9b380279f23289b9b5497fca66&=&format=webp&quality=lossless&width=850&height=850",
     Theme = "Default",
     Title = "Mai Hub",
     Author = "Luna",
