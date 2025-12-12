be3581e33d760d66a7ccbe69db6592ad2f220489
Luna <violetscriptss2@gmail.com>
2025-12-12 09:00:51 -0300

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,5 +1,5 @@
 return {
-    Background = "https://media.discordapp.net/attachments/1443353629095624825/1448358306849751203/da8050322d4e962ffa68dab9b0bfce50.png?ex=693af849&is=6939a6c9&hm=b9627468618092ec97e0568d7db8a8034d205e9b380279f23289b9b5497fca66&=&format=webp&quality=lossless&width=850&height=850",
+    Background = "https://media.discordapp.net/attachments/1400396358976016438/1449007745259802677/da8050322d4e962ffa68dab9b0bfce50.png?ex=693d551f&is=693c039f&hm=dce94dfe7fab5148a2b24eaaa0051a12efaeb0dd3cc6a9c52092687871ac0029&=&format=webp&quality=lossless",
     Theme = "Default",
     Title = "Mai Hub",
     Author = "Luna",
