334417b4d99ade367cc3b1fae88d3567b78535bd
Luna <248779403+LunaS3ven@users.noreply.github.com>
2026-01-14 18:45:34 -0300

    Create ConfigFluent.lua

---
+++ ConfigFluent.lua
@@ -0,0 +1,9 @@
+return {
+    Background = "https://media.discordapp.net/attachments/1400396358976016438/1449007745259802677/da8050322d4e962ffa68dab9b0bfce50.png?ex=693d551f&is=693c039f&hm=dce94dfe7fab5148a2b24eaaa0051a12efaeb0dd3cc6a9c52092687871ac0029&=&format=webp&quality=lossless",
+    Theme = "Darker",
+    Title = "Mai Hub",
+    Author = "Luna",
+    SetBackgroundImageTransparency = 0.7,
+    Discord = "https://discord.com/invite/MArH2ZJSt8",
+    Key = "KeyLess"
+}
